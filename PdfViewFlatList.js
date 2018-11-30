9ef034ede7965cf359660e38bfaeb5a7c6495fbf
Wonday <wonday@wonday.org>
2018-11-30 22:29:11 +0800

    remove log

--- PdfViewFlatList.js
+++ PdfViewFlatList.js
@@ -26,7 +26,6 @@ export default class PdfViewFlatList extends FlatList {
      */
     scrollToXY = (x, y) => {
         this._listRef._scrollRef.scrollTo({x: x, y: y, animated: false});
-        console.log(x, y);
     }
 
 }
