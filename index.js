4fcf25bd4941c71ceec429f02761c7842e2bd602
Wonday <wonday@wonday.org>
2019-05-13 22:55:04 +0800

    Fix encodeURI(source.uri) error when receiving encoded uri

--- index.js
+++ index.js
@@ -252,7 +252,7 @@ export default class Pdf extends Component {
         })
             .fetch(
                 source.method ? source.method : 'GET',
-                encodeURI(source.uri),
+                encodeURI(decodeURI(source.uri)),
                 source.headers ? source.headers : {},
                 source.body ? source.body : ""
             )
