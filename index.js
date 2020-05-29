3c90dcccbbc4ce6cd663f2c4e5e209f9d0231296
Dominik Tenelsen <do.tenelsen@gmail.com>
2020-05-29 08:16:38 +0200

    Fix extracted warnings

--- index.js
+++ index.js
@@ -13,12 +13,13 @@ import {
     requireNativeComponent,
     View,
     Platform,
-    ProgressBarAndroid,
-    ProgressViewIOS,
     ViewPropTypes,
     StyleSheet,
     Image
 } from 'react-native';
+
+import { ProgressBarAndroid } from '@react-native-community/progress-bar-android'
+import { ProgressViewIOS } from '@react-native-community/progress-view'
 
 import RNFetchBlob from 'rn-fetch-blob';
 
