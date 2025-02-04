c9a231f2230197cfe593cc09781e4453b0383556
Abdullah Mzaien <xa300600@hotmail.com>
2025-02-04 16:07:55 +0300

    feat: support rn 77 (#908)

--- android/src/paper/java/com/facebook/react/viewmanagers/RNPDFPdfViewManagerDelegate.java
+++ android/src/paper/java/com/facebook/react/viewmanagers/RNPDFPdfViewManagerDelegate.java
@@ -13,9 +13,10 @@ import android.view.View;
 import androidx.annotation.Nullable;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.uimanager.BaseViewManagerDelegate;
-import com.facebook.react.uimanager.BaseViewManagerInterface;
+import com.facebook.react.uimanager.BaseViewManager;
+import com.facebook.react.uimanager.LayoutShadowNode;
 
-public class RNPDFPdfViewManagerDelegate<T extends View, U extends BaseViewManagerInterface<T> & RNPDFPdfViewManagerInterface<T>> extends BaseViewManagerDelegate<T, U> {
+public class RNPDFPdfViewManagerDelegate<T extends View, U extends BaseViewManager<T, ? extends LayoutShadowNode> & RNPDFPdfViewManagerInterface<T>> extends BaseViewManagerDelegate<T, U> {
   public RNPDFPdfViewManagerDelegate(U viewManager) {
     super(viewManager);
   }
